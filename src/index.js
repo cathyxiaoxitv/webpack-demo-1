e16505a49b67ecbfe02b5549ef32f8600d090045
cathyxiaoxitv <melodyxiaoxi@gmail.com>
2020-08-06 21:02:12 +0900

    modify deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import png from './assets/1.png'
 
 console.log(png)
 
-// const div = document.getElementById('app')
+const div = document.getElementById('app')
 // console.log('div');
 // console.log(div);
 // div.innerHTML = `
