6ce0a11784481a8dadcdef0596c0796b6041ca0c
cathyxiaoxitv <melodyxiaoxi@gmail.com>
2020-08-06 20:51:40 +0900

    update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import png from './assets/1.png'
 
 console.log(png)
 
-const div = document.getElementById('app')
-console.log('div');
-console.log(div);
-div.innerHTML = `
-<img src="${png}">
-    `
+// const div = document.getElementById('app')
+// console.log('div');
+// console.log(div);
+// div.innerHTML = `
+// <img src="${png}">
+//     `
 
 const button = document.createElement('button');
 button.innerText = '懒加载'
